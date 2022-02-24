7f9afca415b798d5671ac2d0e88763fb09747be4
Jose Juan Reyes <neodevelop@gmail.com>
2022-02-24 14:47:59 -0600

    Implments behaviour in FibonacciServer

--- lib/poor_man_otp/fibonacci_server.ex
+++ lib/poor_man_otp/fibonacci_server.ex
@@ -3,9 +3,17 @@ defmodule PoorMan.FibonacciServer do
   Make the Fibonacci Sequence a Server
   """
 
+  @behaviour PoorManOtp.GenericServer
+
   alias PoorManOtp.GenericServer
+  alias PoorMan.Fibonacci
 
   def start() do
     GenericServer.start(__MODULE__)
+  end
+
+  def handle_message({:compute, n}, state) do
+    result = Fibonacci.sequence(n)
+    {:ok, result, state}
   end
 end
