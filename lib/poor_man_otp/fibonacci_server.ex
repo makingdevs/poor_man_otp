486e93248faf7a4408365c4e1aa54a328d34196b
Jose Juan Reyes <neodevelop@gmail.com>
2022-02-24 15:43:59 -0600

    Start FibonacciServer with self and refers in handle_message

--- lib/poor_man_otp/fibonacci_server.ex
+++ lib/poor_man_otp/fibonacci_server.ex
@@ -9,10 +9,10 @@ defmodule PoorMan.FibonacciServer do
   alias PoorMan.Fibonacci
 
   def start() do
-    GenericServer.start(__MODULE__)
+    GenericServer.start(__MODULE__, self(), %{})
   end
 
-  def handle_message({:compute, n}, state) do
+  def handle_message({:compute, n}, _parent, state) do
     result =
       case Map.get(state, n) do
         nil -> Fibonacci.sequence(n)
