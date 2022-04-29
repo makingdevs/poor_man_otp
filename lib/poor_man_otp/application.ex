39a61b57fe99c30a685bee0b4c3af0e55e2d4436
Jose Juan Reyes <neodevelop@gmail.com>
2022-04-29 00:03:37 -0500

    Add a worker in application with a Supervisor for a Pool!

--- lib/poor_man_otp/application.ex
+++ lib/poor_man_otp/application.ex
@@ -2,7 +2,18 @@ defmodule PoorManOtp.Application do
   use Application
 
   def start(_type, _args) do
-    children = []
+    children = [
+      Supervisor.child_spec(
+        {PoorManOtp.PoolQueue,
+         [
+           worker: {PoorMan.FibonacciGenServer, :start_link, []},
+           n_workers: 3,
+           name: PoolFibonacci
+         ]},
+        id: :worker_fibonacci
+      )
+    ]
+
     opts = [strategy: :one_for_one, name: PoorManOtp.Supervisor]
 
     Supervisor.start_link(children, opts)
