e169fd750b5a454e522b8b269c6ec21d3a55e40a
Jose Juan Reyes <neodevelop@gmail.com>
2022-04-29 19:00:04 -0500

    Add Logger to show the current process

--- lib/poor_man_otp/fibonacci_gen_server.ex
+++ lib/poor_man_otp/fibonacci_gen_server.ex
@@ -2,6 +2,7 @@ defmodule PoorMan.FibonacciGenServer do
   use GenServer
 
   alias PoorMan.Fibonacci
+  require Logger
 
   def start_link(default \\ []) do
     GenServer.start_link(__MODULE__, default)
@@ -12,6 +13,7 @@ defmodule PoorMan.FibonacciGenServer do
   end
 
   def handle_call({:compute, n}, _from, state) do
+    Logger.info("Fibonacci sequence for #{n} processed by #{inspect(self())}")
     result = Fibonacci.sequence(n)
     {:reply, result, state}
   end
